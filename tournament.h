37e8a1de982f90d97da7a8697c7f9827e3325227
kubaflo <kubaflo123@gmail.com>
2023-04-08 19:50:44 +0200

    Euro2020 teams added

--- tournament.h
+++ tournament.h
@@ -13,6 +13,7 @@ class Tournament
     private:
         std::vector<Team> allTeams;
         std::vector<Team> selectedTeams;
+        void generateTeams();
         void chooseTeamsForTournament();
 };
 
@@ -26,12 +27,7 @@ void Tournament::chooseTeamsForTournament()
 
 void Tournament::simulateTournament()
 {
-    if(allTeams.size()<4)
-    {
-        std::cout<<"Tournament needs to have 4 teams!"<<std::endl;
-        return;
-    }
-
+    generateTeams();
     chooseTeamsForTournament();
 
     std::cout<<std::endl<<std::endl<<"=====Semifinal I====="<<std::endl;
@@ -48,6 +44,40 @@ void Tournament::simulateTournament()
 
     std::cout<<std::endl<<std::endl<<"WINNER"<<std::endl;
     finalWinner.printTeam();
+}
 
+void Tournament::generateTeams()
+{
+    allTeams.push_back(Team("France", "Didier Deschamps", 80, 85));
+    allTeams.push_back(Team("Spain", "Luis Enrique", 75, 80));
+    allTeams.push_back(Team("Italy", "Roberto Mancini", 70, 85));
+    allTeams.push_back(Team("Portugal", "Fernando Santos", 85, 75));
+    allTeams.push_back(Team("Netherlands", "Frank de Boer", 75, 80));
+    allTeams.push_back(Team("Germany", "Joachim Löw", 85, 80));
+    allTeams.push_back(Team("Belgium", "Roberto Martinez", 90, 75));
+    allTeams.push_back(Team("England", "Gareth Southgate", 80, 85));
+    allTeams.push_back(Team("Switzerland", "Murat Yakin", 70, 75));
+    allTeams.push_back(Team("Denmark", "Kasper Hjulmand", 75, 80));
+    allTeams.push_back(Team("Sweden", "Janne Andersson", 70, 75));
+    allTeams.push_back(Team("Croatia", "Zlatko Dalić", 75, 80));
+    allTeams.push_back(Team("Russia", "Stanislav Cherchesov", 70, 75));
+    allTeams.push_back(Team("Turkey", "Şenol Güneş", 75, 75));
+    allTeams.push_back(Team("Austria", "Franco Foda", 70, 75));
+    allTeams.push_back(Team("Poland", "Paulo Sousa", 80, 75));
+    allTeams.push_back(Team("Scotland", "Steve Clarke", 65, 70));
+    allTeams.push_back(Team("Czech Republic", "Jaroslav Šilhavý", 70, 75));
+    allTeams.push_back(Team("Ukraine", "Andriy Shevchenko", 75, 75));
+    allTeams.push_back(Team("Wales", "Robert Page", 70, 75));
 
+    for (auto team : allTeams) 
+    {
+        for (int i = 0; i < 5; i++) {
+            std::string player_name = "Player " + std::to_string(i+1);
+            int attack = rand() % 91 + 10; // generate a random number between 10 and 100
+            int defence = rand() % 91 + 10; // generate a random number between 10 and 100
+            Position position = Position(i % 3); // alternate between the four positions
+            PlayerStats stats(attack, defence, 0); // set the player's goal count to 0
+            allTeams[i].addPlayer(Player(player_name, stats, position));
+        }
+    }
 }
